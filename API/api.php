7fe3f73d413e8b369b39cd191ef8c937f5d71a5c
driss292 <drisskaci@outlook.fr>
2022-10-04 11:28:24 +0200

    path img

--- API/api.php
+++ API/api.php
@@ -1,10 +1,17 @@
 <?php
+define("URL", str_replace("index.php", "", (isset($_SERVER['HTTPS']) ? "https" : "http") .
+    "://" . $_SERVER['HTTP_HOST'] . $_SERVER["PHP_SELF"]));
+
+
 function getFormations() {
     $pdo = getConnexion();
     $req = "SELECT f.id, f.libelle, f.description, f.image, c.libelle as 'categorie' FROM formation f INNER JOIN categorie c on f.categorie_id = c.id";
     $stmt = $pdo->prepare($req);
     $stmt->execute();
     $formations = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    for ($i = 0; $i < count($formations); $i++) {
+        $formations[$i]["image"] = URL . "images/cours/" . $formations[$i]["image"];
+    }
     $stmt->closeCursor();
     sendJSON($formations);
 }
@@ -16,6 +23,9 @@ function getFormationsByCategorie($categorie) {
     $stmt->bindValue(":categorie", $categorie, PDO::PARAM_STR);
     $stmt->execute();
     $formations = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    for ($i = 0; $i < count($formations); $i++) {
+        $formations[$i]["image"] = URL . "images/cours/" . $formations[$i]["image"];
+    }
     $stmt->closeCursor();
     sendJSON($formations);
 }
@@ -27,9 +37,10 @@ function getFormationById($id) {
     $stmt->bindValue(":id", $id, PDO::PARAM_INT);
 
     $stmt->execute();
-    $formations = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $formation = $stmt->fetch(PDO::FETCH_ASSOC);
     $stmt->closeCursor();
-    sendJSON($formations);
+    $formation["image"] = URL . "images/cours/" . $formation["image"];
+    sendJSON($formation);
 }
 
 function getConnexion() {
