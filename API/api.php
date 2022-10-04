a71a4eed1c73b759b163cc21fc05206c6deee4b2
driss292 <drisskaci@outlook.fr>
2022-10-04 11:02:19 +0200

    Call BDD

--- API/api.php
+++ API/api.php
@@ -1,12 +1,43 @@
 <?php
 function getFormations() {
-    echo "Liste des formations";
+    $pdo = getConnexion();
+    $req = "SELECT f.id, f.libelle, f.description, f.image, c.libelle as 'categorie' FROM formation f INNER JOIN categorie c on f.categorie_id = c.id";
+    $stmt = $pdo->prepare($req);
+    $stmt->execute();
+    $formations = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $stmt->closeCursor();
+    sendJSON($formations);
 }
 
 function getFormationsByCategorie($categorie) {
-    echo "Liste des formations par catégories";
+    $pdo = getConnexion();
+    $req = "SELECT f.id, f.libelle, f.description, f.image, c.libelle as 'categorie' FROM formation f INNER JOIN categorie c on f.categorie_id = c.id WHERE c.libelle = :categorie";
+    $stmt = $pdo->prepare($req);
+    $stmt->bindValue(":categorie", $categorie, PDO::PARAM_STR);
+    $stmt->execute();
+    $formations = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $stmt->closeCursor();
+    sendJSON($formations);
 }
 
 function getFormationById($id) {
-    echo "Affichage d'une formation";
+    $pdo = getConnexion();
+    $req = "SELECT f.id, f.libelle, f.description, f.image, c.libelle as 'categorie' FROM formation f INNER JOIN categorie c on f.categorie_id = c.id WHERE f.id = :id";
+    $stmt = $pdo->prepare($req);
+    $stmt->bindValue(":id", $id, PDO::PARAM_INT);
+
+    $stmt->execute();
+    $formations = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $stmt->closeCursor();
+    sendJSON($formations);
+}
+
+function getConnexion() {
+    return new PDO("mysql:host=localhost;dbname=fh2prog;chartset=uth8", "root", "root");
+}
+
+function sendJSON($infos) {
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json");
+    echo json_encode($infos, JSON_UNESCAPED_UNICODE);
 }
